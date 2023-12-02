1b3fddf54578c9b6f4c54c6dc6fa7fcdf97c3442
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-12-02 20:45:32 +0300

    Initial commit

---
+++ app/src/main/java/ru/predprof/trackingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.predprof.trackingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.coroutines.coroutineScope
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

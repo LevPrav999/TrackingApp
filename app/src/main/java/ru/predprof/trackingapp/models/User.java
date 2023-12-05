d2067943501b2662ed1e68a52f5cbe0223cc493e
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-12-05 11:47:24 +0300

    add user model

---
+++ app/src/main/java/ru/predprof/trackingapp/models/User.java
@@ -0,0 +1,11 @@
+package ru.predprof.trackingapp.models;
+
+public class User {
+    double height = 1;
+    double weight= 1;
+    double imt = weight /  (height*height);
+
+    int personalSkills = 0;
+    int healthStatus = 0;
+
+}

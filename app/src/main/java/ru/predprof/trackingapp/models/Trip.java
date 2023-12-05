479907454802eab8102ca8e9db228a2029f60a09
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-12-05 11:34:40 +0300

    add room db for trips and shared preferences for user data

---
+++ app/src/main/java/ru/predprof/trackingapp/models/Trip.java
@@ -0,0 +1,37 @@
+package ru.predprof.trackingapp.models;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Trip {
+    @PrimaryKey(autoGenerate = true)
+    public int number;
+
+    @ColumnInfo(name = "len_km")
+    public String lenKm; // Длительность в километрах
+
+    @ColumnInfo(name = "time")
+    public String time; // Время поездки
+
+    @ColumnInfo(name = "avg_speed")
+    public String avgSpeed; // Средняя скорость
+
+    @ColumnInfo(name = "max_speed")
+    public String maxSpeed; // Максимальная скорость
+
+    @ColumnInfo(name = "data_pulse")
+    public ArrayList<String> dataPulse; // Данные о пульсе
+
+    @ColumnInfo(name = "dif_auto")
+    public String difficultAuto; // Сложность расчётная
+
+    @ColumnInfo(name = "dif_real")
+    public String difficultReal; // Сложность реальная
+
+}
---
+++ app/src/main/java/ru/predprof/trackingapp/room/TripDao.java
@@ -0,0 +1,27 @@
+package ru.predprof.trackingapp.room;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import ru.predprof.trackingapp.models.Trip;
+
+@Dao
+public interface TripDao {
+    @Query("SELECT * FROM trip")
+    List<Trip> getAll();
+
+    @Query("SELECT * FROM trip WHERE number IN (:userIds)")
+    List<Trip> loadAllByIds(int[] userIds);
+
+
+    @Insert
+    void insertAll(Trip... users);
+
+    @Delete
+    void delete(Trip user);
+}
+

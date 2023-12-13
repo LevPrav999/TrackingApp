c66fe4403de6a0b4178414676f3e70598fc07471
apium <gbogdan2007@gmail.com>
2023-12-13 21:05:46 +0300

    delete dub

--- app/src/main/java/ru/predprof/trackingapp/MainActivity.java
+++ app/src/main/java/ru/predprof/trackingapp/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
             );
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

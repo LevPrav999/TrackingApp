20f167d8b99718006af22b25f533efada7ef77f1
Karaseyka <vladchuprina12@mail.ru>
2023-12-24 15:48:45 +0300

    add NoBarActivity

---
+++ app/src/main/java/ru/predprof/trackingapp/NoBarActivity.java
@@ -0,0 +1,28 @@
+package ru.predprof.trackingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import ru.predprof.trackingapp.databinding.NoBarActivityBinding;
+
+public class NoBarActivity  extends AppCompatActivity {
+    NoBarActivityBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = NoBarActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+    public void replace(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.replace(R.id.map, fragment).commit();
+
+    }
+}
